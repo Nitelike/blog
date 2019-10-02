c51990c7f3a8f5b83dd5f8faa0aaba44816a54c8
Khritonenko <ax.2904@gmail.com>
2019-10-02 16:58:50 +0300

    responsive top navbar

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<nav>
+  <ul class="topmenu">
+    <li>
+      <a href="#">HOME</a>
+    </li>
+    <li class="down">
+      <a href="#">DOWNLOADS &#5121;</a>
+      <ul class="submenu">
+        <li>
+          <a href="#">OLD VERSIONS</a>
+        </li>
+        <li>
+          <a href="#">NEW VERSIONS</a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">DOCUMENTATION</a>
+    </li>
+    <li>
+      <a href="#">ABOUT US</a>
+    </li>
+    <li class="menu_icon">
+      <a href="#">&#9776;</a>
+    </li>
+  </ul>
+</nav>
--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>HOME PAGE</title>
-	<link rel="stylesheet" type="text/css" href="css/topnav.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="js/jquery-3.4.1.min.js"></script>
-	<?php include("php/connect_to_db.php"); ?>
+	<link rel="stylesheet" type="text/css" href="css/topnav.css?version=3.5">
+	<link rel="stylesheet" type="text/css" href="css/style.css?version=3.3">
 </head>
 <body>	
-	<nav id="topnav"></nav>
-
-	<script src="js/load.js"></script>
+	<?php include 'includes/header.php' ?>
 </body>
 </html>
