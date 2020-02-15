5dc63c43d41b0c2dc7f7ea6d36a751110ac4fcab
Nitelike <ax.2904@gmail.com>
2020-02-15 15:06:28 +0300

    fixed bug when member can upload images

--- app/controllers/uploadsController.php
+++ app/controllers/uploadsController.php
@@ -10,14 +10,17 @@ class Uploads_Controller extends Controller
 
 	public function upload()
 	{
-		$response = array('errors' => array(''), 'success' => array(''));
-
-		if(isset($_POST["submit"]))
+		if(isset($_SESSION['user']) and $_SESSION['user']['status'] !== 'member')
 		{
-			$uploads_model = $this->model('Uploads');
-			$response = $uploads_model->upload_image();
+			$response = array('errors' => array(''), 'success' => array(''));
+
+			if(isset($_POST["submit"]))
+			{
+				$uploads_model = $this->model('Uploads');
+				$response = $uploads_model->upload_image();
+			}
+			$this->view(array('user/userMenu', 'testUpload'), 'generalTemplate', array('page' => 'Загрузить', 'response' => $response));
 		}
-		$this->view(array('user/userMenu', 'testUpload'), 'generalTemplate', array('page' => 'Загрузить', 'response' => $response));
 	}
 
 }
